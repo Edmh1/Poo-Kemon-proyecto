92540b9b49930cdc0211e227588c4d90cd7d560e
vladimir <vladimir_6606@hotmail.com>
2024-09-23 19:50:16 -0500

    empezando ahi creador

--- SourcePackPookemon/registro/Organizador.java
+++ SourcePackPookemon/registro/Organizador.java
@@ -1,5 +1,29 @@
 package SourcePackPookemon.registro;
 
+import java.util.ArrayList;
+
+import SourcePackPookemon.torneo.Torneo;
+
 public class Organizador {
+
+    private int idOrganizador;
+    ArrayList<Torneo> torneos = new ArrayList<>();
+    private int torneoActual;
+
+    public Organizador(int idOrganizador, int torneoActual) {
+        this.idOrganizador = idOrganizador;
+        this.torneoActual = torneoActual;
+    }
+
+    @Override
+    public String toString() {
+        return "Organizador [idOrganizador=" + idOrganizador + ", torneoActual=" + torneoActual + "]";
+    }
+
+    public void inscribirJugadorTorneo(Entrenador jugador , int idTorneo){
+       
+    }
+
+    
 
 }
