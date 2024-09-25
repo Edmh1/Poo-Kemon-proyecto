392832b2037ebe899348b24a1f48017895fcb7b6
AndresMes <federicoescobar2022@gmail.com>
2024-09-25 14:10:27 -0500

    Agregados metodos de gestión para pookemons en Torneo

--- appPookemon/src/torneo/Torneo.java
+++ appPookemon/src/torneo/Torneo.java
@@ -1,15 +1,55 @@
 package torneo;
 
 import java.util.ArrayList;
+import pookemon.Efecto;
+import pookemon.Movimiento;
 
 import registro.Entrenador;
+import pookemon.Pookemon;
 
 public class Torneo {
 
     private ArrayList<Entrenador> jugadores = new ArrayList<>();
+    private ArrayList<Pookemon> pookemones = new ArrayList<>();
 
     public void addJugador(Entrenador e){
         jugadores.add(e);
+    }
+    
+    public void addPookemon(Pookemon x){
+        pookemones.add(x);
+    }
+    
+    public void editarNombrePookemon(int idPookemon_p, String nuevoNombre){
+        for (Pookemon p : pookemones) {
+            if(p.getIdPookemon() == idPookemon_p){
+                p.setNombre(nuevoNombre);
+            }
+        }
+    }
+    
+    public void editarElementoPookemon(int idPookemon_p, String nuevoElemento){
+        for (Pookemon p : pookemones) {
+            if(p.getIdPookemon() == idPookemon_p){
+                p.setElemento(nuevoElemento);
+            }
+        }
+    }
+    
+    public void editarEfectoPookemon(int idPookemon_p, Efecto nuevoEfecto){
+        for (Pookemon p : pookemones) {
+            if(p.getIdPookemon() == idPookemon_p){
+                p.setEfecto(nuevoEfecto);
+            }
+        }
+    }
+    
+    public void eliminarPookemon(int id_pookemon_p){
+        for (Pookemon p : pookemones) {
+            if(p.getIdPookemon() == id_pookemon_p){
+                pookemones.remove(p);
+            }
+        }
     }
 
     @Override
