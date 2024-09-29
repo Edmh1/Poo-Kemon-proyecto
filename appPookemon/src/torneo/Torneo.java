63e6354cf120dfbbb4789479f47aafb086391354
AndresMes <federicoescobar2022@gmail.com>
2024-09-29 08:39:54 -0500

    Gestión de movimientos en Torneo

--- appPookemon/src/torneo/Torneo.java
+++ appPookemon/src/torneo/Torneo.java
@@ -12,6 +12,7 @@ public class Torneo {
 
     private ArrayList<Entrenador> jugadores = new ArrayList<>();
     private ArrayList<Pookemon> pookemones = new ArrayList<>();
+    private ArrayList<Movimiento> movimientos = new ArrayList<>();
 
     public void addJugador(Entrenador e){
         jugadores.add(e);
@@ -54,22 +55,44 @@ public class Torneo {
         }
     }
     
-    public void addMovimientoAPookemon(Movimiento x, int idPookemon_p){
-        for (Pookemon p : pookemones) {
-            if(p.getIdPookemon() == idPookemon_p){
-                p.addMovimiento(x);
+    public void addMovimiento(Movimiento x){
+        movimientos.add(x);
+    }
+    
+    
+    
+    public void eliminarMovimiento(int id_movimiento_p){
+        for (int i = 0; i < movimientos.size(); i++) {
+            if(movimientos.get(i).getIdMovimiento() == id_movimiento_p){
+                movimientos.remove(i);
             }
+            
         }
     }
     
-    public void eliminarMovimientoAPookemon(int idMovimiento_p, int idPookemon_p){
-        for (Pookemon p : pookemones) {
-            if(p.getIdPookemon() == idPookemon_p){
-                List<Movimiento> movimientosP = p.getMovimientos();
-                for (int i = 0; i < movimientosP.size(); i++) {
-                    if(movimientosP.get(i).getIdMovimiento() == idMovimiento_p){
-                        movimientosP.remove(i);
-                    }       
+    public void editarMovimiento(int id_movimiento_p, String newNombre, String newDescripcion, int newPotencia, int newPrecision, int newCantidadPP, String newElemento, Efecto newEfecto){
+        for (Movimiento m : movimientos) {
+            if(m.getIdMovimiento() == id_movimiento_p){
+                if(newNombre != null){
+                    m.setNombreMovimiento(newElemento);
+                }
+                if(newDescripcion != null){
+                    m.setDescripcionMovimiento(newDescripcion);
+                }
+                if(newPotencia != 0){
+                    m.setPotencia(newPotencia);
+                }
+                if(newPrecision != 0){
+                    m.setPrecision(newPrecision);
+                }
+                if(newCantidadPP != 0){
+                    m.setCantidadPP(newCantidadPP);
+                }
+                if(newElemento != null){
+                    m.setElemento(newElemento);
+                }
+                if(newEfecto != null){
+                    m.setEfecto(newEfecto);
                 }
             }
         }
@@ -84,6 +107,16 @@ public class Torneo {
 
         return lis;
     }
+    
+    public String toStringMovimientos() {
+        String lis = "Lista de movimientos:\n";
+        
+        for (Movimiento m : movimientos) {
+            lis +="- " + m.toString()+ "\n";
+        }
+
+        return lis;
+    }
 
     
     public String toStringJugadores() {
@@ -94,6 +127,20 @@ public class Torneo {
         }
 
         return lis;
+    }
+
+    /**
+     * @return the movimientos
+     */
+    public ArrayList<Movimiento> getMovimientos() {
+        return movimientos;
+    }
+
+    /**
+     * @param movimientos the movimientos to set
+     */
+    public void setMovimientos(ArrayList<Movimiento> movimientos) {
+        this.movimientos = movimientos;
     }
 
 }
