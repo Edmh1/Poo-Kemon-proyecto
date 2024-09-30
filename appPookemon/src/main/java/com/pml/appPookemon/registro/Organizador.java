3b23a114fcaa90288d9a2a68618e4f21c0db6224
vladimir <vladimir_6606@hotmail.com>
2024-09-30 16:18:10 -0500

    lista de torneos al organizador

--- appPookemon/src/main/java/com/pml/appPookemon/registro/Organizador.java
+++ appPookemon/src/main/java/com/pml/appPookemon/registro/Organizador.java
@@ -1,11 +1,12 @@
 package main.java.com.pml.appPookemon.registro;
 
 import main.java.com.pml.appPookemon.torneo.Torneo;
-
+import java.util.ArrayList;
 
 public class Organizador {
     private int idOrganizador;
     private Torneo torneoActual;
+    private ArrayList<Torneo> torneos = new ArrayList<>();  
     private String password;
 
     public Organizador(int idOrganizador, String password) {
@@ -17,9 +18,20 @@ public class Organizador {
     public String toString() {
         return "Organizador [idOrganizador=" + idOrganizador + "]";
     }
+    public String mostrarTorneos(){
+        String lis = "";
+        for (Torneo torneo : torneos) {
+            lis += torneo.toString() + "\n";
+        } 
+        return lis;
+    }
 
     public void inscribirJugadorTorneo(Entrenador jugador ){
         torneoActual.addJugador(jugador);
+    }
+
+    public void añadirTorneos(){
+        torneos.add(torneoActual);
     }
 
     public Torneo crearTorneo(){
