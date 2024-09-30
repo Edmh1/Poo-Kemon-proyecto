28f35073569116e92d9baea46a98804b5066619f
vladimir <vladimir_6606@hotmail.com>
2024-09-30 16:58:48 -0500

    recompensa

--- appPookemon/src/main/java/com/pml/appPookemon/torneo/Recompensa.java
+++ appPookemon/src/main/java/com/pml/appPookemon/torneo/Recompensa.java
@@ -1,5 +1,35 @@
 package main.java.com.pml.appPookemon.torneo;
 
 public class Recompensa {
+   private int id; 
+   private  String nombre;
+   private  String descripcion;
+   private String fechadevencimiento;
 
+
+
+   
+    @Override
+    public String toString() {
+        return "Recompensa [id=" + id + ", nombre=" + nombre + ", descripcion=" + descripcion + ", fechadevencimiento="
+                + fechadevencimiento + "]";
+    }
+    public Recompensa(int id, String nombre, String descripcion, String fechadevencimiento) {
+        this.id = id;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.fechadevencimiento = fechadevencimiento;
+    } 
+
+
+   
+
+  
+
+   
+
+   
+
+
+    
 }
