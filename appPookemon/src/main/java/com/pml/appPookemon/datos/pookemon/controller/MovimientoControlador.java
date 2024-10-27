209bfec5ff26c6faeb6901d7fee3680e9b029867
arturoandresv <96695625+arturoandresv@users.noreply.github.com>
2024-10-27 17:24:55 -0500

    Add files via upload

---
+++ appPookemon/src/main/java/com/pml/appPookemon/datos/pookemon/controller/MovimientoControlador.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main.java.com.pml.appPookemon.datos.pookemon.controller;
+
+import main.java.com.pml.appPookemon.datos.pookemon.Efecto;
+import main.java.com.pml.appPookemon.datos.pookemon.model.Movimiento;
+import main.java.com.pml.appPookemon.datos.pookemon.model.MovimientoEspecial;
+import main.java.com.pml.appPookemon.datos.pookemon.model.MovimientoFisico;
+
+/**
+ *
+ * @author Arturo
+ */
+public class MovimientoControlador {
+
+    public Movimiento agregarMovimiento(String nombreMovimiento, int potencia, int precision, int cantidadPP, Efecto efecto, String elemento, String tipo) {
+        if(tipo.equalsIgnoreCase("Mov. Fisico")){
+            Movimiento movimiento = new MovimientoFisico(0, nombreMovimiento, potencia, precision, cantidadPP, elemento, efecto);
+            return movimiento;
+        } else {
+            Movimiento movimiento = new MovimientoEspecial(0, nombreMovimiento, potencia, precision, cantidadPP, elemento, efecto);
+            return movimiento;
+        }
+        
+    }
+    
+}
